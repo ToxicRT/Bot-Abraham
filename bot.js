b795139239c905a685cf68f8ae13e3f198b3d286
ToxicRT <43280922+ToxicRT@users.noreply.github.com>
2018-10-24 17:44:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'i need help') {
+    	message.reply('For what ?');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
