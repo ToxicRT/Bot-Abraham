ea3084a817208269b87d1f3ebf31303e585a3b49
ToxicRT <43280922+ToxicRT@users.noreply.github.com>
2018-10-25 09:16:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,18 +2,26 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Ready D:!');
+    bot.user.setActivity("No Prefix :/", {type: "WATCHING"});
+    
 });
 
 client.on('message', message => {
     if (message.content === 'Hello') {
-    	message.reply('hi mother fuckers :D');
+    	message.reply('hi mother fucker :D');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'i need help') {
     	message.reply('For what ?');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'string don't work') {
+    	message.reply('dm @AbrahaMatheus#9020 he will give you a solution :D');
   	}
 });
 
